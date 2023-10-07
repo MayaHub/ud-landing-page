11077f4999e9c66d702451ea6b1af0e551c976db
Maya <maya.hubbard@outlook.com>
2023-10-06 22:01:39 -0700

    Still trying to get scroll to work

--- js/app.js
+++ js/app.js
@@ -13,142 +13,70 @@
  *
  */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
-const navList = document.querySelector("#navbar__list"); //select ul element
-// const landingContainer = document.querySelector(".landing__container");
-const sectionHeader = document.querySelectorAll("h2"); //select all h2 elements
-const sections = document.querySelectorAll("section"); //select all section elements
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-//add class name to section headers
-sectionHeader.forEach(header => {
-  header.classList.add("sec__hdr");
- });
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
 // build the nav
 
- //select section headers and create li elements with inner text of header elements
-sections.forEach(section => {
+const navList = document.querySelector("#navbar__list"); //select ul element
+const sections = document.querySelectorAll("section"); //select all section elements
+
+//select section headers and create li elements with inner text of header elements
+sections.forEach((section) => {
   const navItem = document.createElement("li"); //create li element
   const navLink = document.createElement("a"); //create link element
   navLink.innerText = section.dataset.nav; //use data-nav to find sections and add text
   navLink.href = `#${section.id}`; //make clickable link to section by data-nav
-  navItem.appendChild(navLink); //add li elements to ul 
+  navItem.appendChild(navLink); //add li elements to ul
   navList.appendChild(navItem); //add link elements to li's
 
   navLink.classList.add("menu__link"); //add class name to li element
 });
 
-//Smooth scroll
+// Scroll to anchor ID using scrollTO event
 const links = document.querySelectorAll(".menu__link"); //get all of the a elements
 
-//add the click event
-links.forEach(link => {
-  link.addEventListener("click", smooth);
-})
+//add the click event to each a element
+links.forEach((link) => {
+  link.addEventListener("click", smooth); //use smooth function for click event behavior
+});
 
 //function for the click event
 function smooth(event) {
   event.preventDefault(); //stop the click from doing its standard behavior
-  const href = this.getAttribute("href"); //find the href element tags 
-  
-  document.querySelector(href).scrollIntoView({ //set scroll behavior to smooth
-    behavior: "smooth"
+  const href = this.getAttribute("href"); //find the href element tags
+
+  document.querySelector(href).scrollIntoView({
+    //set scroll behavior to smooth on each href
+    behavior: "smooth",
   });
 }
 
-
-
-
-
 // Add class 'active' to section when near top of viewport
 
-// function sectionInView(section) {
-//   const inView = document.documentElement.clientHeight;
-    // section.getBoundingClientRect();
-//   return (
-//     inView.top >= 0 && inView.left >= 0 &&
-//     inView.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//     inView.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// }
-
-// function setActiveSection() {
-//   sections.forEach(section => {
-//     if (sectionInView(section)) {
-//       section.classList.add("your-active-class");
-//     } else {
-//       section.classList.remove("your-active-class")
-//     }
-//   });
-// }
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-
 // Set sections as active
-
 
 /** viewport research/practice */
 
 const viewportHeight = document.documentElement.clientHeight;
-const viewportWidth = document.documentElement.clientWidth;
-
-const sectionOne = document.getElementById("section1");
-const topEdge = sectionOne.getBoundingClientRect().top;
-const bottomEdge = sectionOne.getBoundingClientRect().bottom;
-
+const headers = document.querySelectorAll("h2");
 
 //determine if element is in viewport
-function isInView() {
-    if (sectionOne.topEdge < viewportHeight && sectionOne.bottomEdge > 0) {
-      sectionOne.style.backgroundColor = "green";
+function isInView(section) {
+  const bound = section.getBoundingClientRect();
+  return bound.top < viewportHeight && bound.bottom > 0;
+}
+
+window.addEventListener("scroll", function (e) {
+  sections.forEach((section) => {
+    section.classList.remove("your-active-class");
+    if(isInView(section)) {
+      section.classList.add("your-active-class");
     }
-  }
-
-// document.addEventListener("scroll", function() {
-//   sections.forEach(section => {
-//     if(isInView(section)) {
-//       section.style.backgroundColor = "green";
-//     }
-//   });
-// }
+  });
+});
 
 
-  // when scrolling, how much of the element must be visible for the effect to take place?
-  // What is a good percentage to use if more than one element is in the viewport? Highest one is highlighted?
-  // while the top of the section or the bottom of the section comes into the viewport
-    // ****use boolean function isInView(element) for while statement****
-  // change state to active 
-  // change background to highlight
+// when scrolling, how much of the element must be visible for the effect to take place?
+// What is a good percentage to use if more than one element is in the viewport? Highest one is highlighted?
+// while the top of the section or the bottom of the section comes into the viewport
+// ****use boolean function isInView(element) for while statement****
+// change state to active
+// change background to highlight
